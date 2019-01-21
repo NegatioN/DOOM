1e9f86fc62f995e311b3be394db0fa14bde361a8
NegatioN <joakimrishaug@gmail.com>
2019-01-22 00:11:37 +0100

    Add a way to start the game

--- linuxdoom-1.10/compile.sh
+++ linuxdoom-1.10/compile.sh
@@ -2,3 +2,12 @@
 mkdir linux
 sudo apt-get install libxext-dev:i386 -y
 make
+
+# You need to copy a doom1.wad file into your linx/-folder. case sensitive.
+# https://ftp.fau.de/aminet/docs/help/Doom_FAQ03.txt for explanation of Error: R_TextureNumForName: SW1BLUE not found
+
+
+#The quickest way to run is probably Xephyr? Doesn't render colors properly or stretch out screen
+sudo apt-get install xserver-xephyr -y
+# Xephyr :1 -ac -screen 320x200x8 &
+# DISPLAY=:1 xterm
